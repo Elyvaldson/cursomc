2055902dd14559d3793974ffc691f5fce38d0352
Elyvaldson <elyvaldson@hotmail.com>
2020-08-10 12:03:16 -0300

    Classe Produto e relacao com Categoria

---
+++ src/main/java/com/elyvaldson/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.elyvaldson.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.elyvaldson.cursomc.domain.Categoria;
+import com.elyvaldson.cursomc.repositories.CategoriaRepository;
+import com.elyvaldson.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id){
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+id+", Tipo: "+Categoria.class.getName()));
+	}
+}
